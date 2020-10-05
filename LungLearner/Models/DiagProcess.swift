5f673d2dcd02a33dc803e5cccbf5801794a84903
Drew - Mac <hirseljmajr@gmail.com>
2020-10-04 20:04:52 -0400

    adding gitignore file

---
+++ LungLearner/Models/DiagProcess.swift
@@ -0,0 +1,8 @@
+//
+//  DiagProcess.swift
+//  LungLearner
+//
+//  Created by Drew Hirselj on 10/4/20.
+//
+
+import Foundation
---
+++ LungLearner/Views/Incorr.swift
@@ -0,0 +1,20 @@
+//
+//  Incorr.swift
+//  LungLearner
+//
+//  Created by Drew Hirselj on 10/4/20.
+//
+
+import SwiftUI
+
+struct Incorr: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Incorr_Previews: PreviewProvider {
+    static var previews: some View {
+        Incorr()
+    }
+}
