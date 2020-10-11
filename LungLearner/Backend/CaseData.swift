2bcfbb041cc1c5386444e316c10cddb93584da12
aunghoo <aung@umich.edu>
2020-10-11 13:20:45 -0400

    Made some Database modifications to better fit data criteria and naming

--- LungLearner/Backend/CaseData.swift
+++ LungLearner/Backend/CaseData.swift
@@ -43,19 +43,19 @@ enum CaseError: Error {
 func makeCase(caseInfo: Row) -> CaseData {
     let ID = Expression<Int64>("ID")
     let CorrectDiagnosis = Expression<String>("CorrectDiagnosis")
-    let Persona = Expression<String>("Persona")
+    let Persona = Expression<String>("History")
     let Temperature = Expression<Double>("Temperature")
     let HeartRate = Expression<Double>("HeartRate")
     let RespiratoryRate = Expression<Double?>("RespiratoryRate")
-    let BloodPressure = Expression<String>("Bloodpressure")
-    let OxygenSaturation = Expression<String>("Oxygensaturation")
+    let BloodPressure = Expression<String>("BloodPressure")
+    let OxygenSaturation = Expression<String>("OxygenSaturation")
     let General = Expression<String>("General")
     let Cardiovascular = Expression<String>("Cardiovascular")
     let Chest = Expression<String>("Chest")
-    let BloodGas = Expression<String>("bloodgas")
-    
+    let BloodGas = Expression<String>("Bloodgas")
+
     let exam = Exam(general:caseInfo[General],cardiovascular:caseInfo[Cardiovascular], chest: caseInfo[Chest])
-    let symptom = Symptom(temperature:caseInfo[Temperature], heartRate:caseInfo[HeartRate], respiratoryRate:caseInfo[RespiratoryRate] ?? 0.0, bloodPressure:caseInfo[BloodPressure], oxygenSaturation:caseInfo[OxygenSaturation])
+    let symptom = Symptom(temperature:caseInfo[Temperature], heartRate:caseInfo[HeartRate], respiratoryRate:caseInfo[RespiratoryRate] ?? 23.0, bloodPressure:caseInfo[BloodPressure], oxygenSaturation:caseInfo[OxygenSaturation])
     let caseData = CaseData(id:Int(caseInfo[ID]), history:caseInfo[Persona], correctDiagnosis:caseInfo[CorrectDiagnosis], symptoms:symptom, exam:exam, bloodGas:caseInfo[BloodGas])
     return caseData
 }
