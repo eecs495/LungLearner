c96e15a99a4bf61933baa9fb4c0abe1e8276bcec
katherinesanchez <33733620+katherinesanchez@users.noreply.github.com>
2020-11-16 10:33:55 -0500

    Update LungLearnerTests.swift

--- LungLearnerTests/LungLearnerTests.swift
+++ LungLearnerTests/LungLearnerTests.swift
@@ -4,52 +4,47 @@
 //
 //  Created by Katherine Sanchez on 11/10/20.
 //
-
 import XCTest
-var sut: CaseDataManager
-var ssut: UserData
+
 
 class LungLearnerTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        sut = CaseDataManager()
-        ssut = UserDatabaseManager()
     }
 
     override func tearDown() {
-        sut = nil
-        ssut = nil
         super.tearDown()
     }
 
     func testGetRandomCase() {
       // 1. given
       //let guess = sut.targetValue + 5
-
       // 2. when
-      sut.getRandomCase()
-
+      let sut: CaseDatabaseManager = CaseDatabaseManager()
+      let case1 = try! sut.getRandomCase()
+      //printCase(caseInfo: case1.caseId)
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
+      XCTAssertGreaterThan(case1.caseId, 0, "Random Case")
     }
 
     func testGetCaseById() {
       // 1. given
       let c = 10
+      let sut: CaseDatabaseManager = CaseDatabaseManager()
 
       // 2. when
-      sut.getCaseById(id: c)
+      let case1 = try! sut.getCaseById(Id: Int64(c))
 
       // 3. then
-      XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(case1.id, 10, "returned wrong case id")
     }
 
     func testGetAllCases() {
       // 1. given
       //let c = 10
-
       // 2. when
+      let sut: CaseDatabaseManager = CaseDatabaseManager()
       sut.getAllCases()
 
       // 3. then
@@ -59,8 +54,8 @@ class LungLearnerTests: XCTestCase {
     func testPrepareUserDatabase() {
       // 1. given
       //let c = 10
-
       // 2. when
+      let ssut: UserDatabaseManager = UserDatabaseManager()
       ssut.prepareUserDatabase()
 
       // 3. then
@@ -70,19 +65,21 @@ class LungLearnerTests: XCTestCase {
     func testGetTotalUserProgress() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getTotalUserProgress()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      let progress = ssut.getTotalUserProgress()
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(progress.correctCases, 0, "returned wrong case id")
     }
 
     func testStoreCaseResult() {
       // 1. given
-      let r = 10
-
+      
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+        let r: UserCaseResult = UserCaseResult(caseid: 25, diagnoses: ["COPD"], reason: "image", correct: true)
       // 2. when
+      
       ssut.storeCaseResult(result: r)
 
       // 3. then
@@ -92,9 +89,9 @@ class LungLearnerTests: XCTestCase {
     func testGetListOfCompletedCases() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getListOfCompletedCases()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+        _ = ssut.getListOfCompletedCases()
 
       // 3. then
       //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
@@ -102,52 +99,53 @@ class LungLearnerTests: XCTestCase {
     func testSetCaseFavorite() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.setCaseFavorite(idInput: 10, favoriteInput: True)()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+        let x = ssut.setCaseFavorite(idInput: 10, favoriteInput: true)
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(x, true, "incorrectly set favorite")
     }
     func testGetFavoriteCases() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getFavoriteCases()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      let x = ssut.getFavoriteCases()
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(x, [], "wrong cases returned")
     }
 
     func testgetUserCaseById() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getUserCaseById(Id: 10)
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      let x = try! ssut.getUserCaseById(Id: 10)
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(x.caseid, 10, "returned wrong case")
     }
 
     func testGetUserDiagnosis() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getUserDiagnosis()
+      let id = 10;
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      let x = ssut.getUserDiagnosis(idInput: Int64(id))
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      XCTAssertEqual(x.diagnoses, "", "returned wrong diagnoses")
     }
 
     func testGetAgeGroup() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getAgeGroup()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      _ = ssut.getAgeGroup()
 
       // 3. then
       //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
@@ -156,17 +154,15 @@ class LungLearnerTests: XCTestCase {
     func testGetGenderGroup() {
       // 1. given
       //let c = 10
-
       // 2. when
-      ssut.getGenderGroup()
+      let ssut: UserDatabaseManager = UserDatabaseManager()
+      _ = ssut.getGenderGroup()
 
       // 3. then
-      //XCTAssertEqual(sut.scoreRound, 10, "returned wrong case id")
+      //XCTAssertGreaterThan(x, 0, "returned wrong case id")
     }
 
 
 
 
 }
-
-
